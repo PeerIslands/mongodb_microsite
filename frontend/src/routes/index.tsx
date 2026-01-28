e99125cca807dca51cb3254664a6b35a7f2c96d8
AkshayAnoopPeerIslands <akshay.anoop@peerislands.io>
2026-01-28 18:12:42 +0530

    Newsletter changes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -67,7 +67,7 @@ const router = createBrowserRouter([
             ),
           },
           {
-            path: '/offerings',
+            path: ROUTES.OFFERINGS,
             element: (
               <Suspense fallback={<PageLoader />}>
                 <OfferingsPage />
